00d0537da771f2952c87dd3ce8841fb695dd7e91
hinfsynz <szhang.asu@gmail.com>
2022-10-04 20:58:54 -0400

    create the working directory and update the path
    create the working directory for slurm and update the path to these scripts

--- run-simulation-scripts/batch_run.sh
+++ run-simulation-scripts/batch_run.sh
@@ -5,16 +5,18 @@
 #   sbatch $HOME/submit_job.sh wecc
 #done
 
+mkdir $HOME/slurm_workdir
+
 for bus in {1..2}
 do
     for tc in $(seq 1.1 .01667 1.2)
     do
         if [ "$#" -lt 1 ]
         then
-    	    sbatch -D $HOME/slurm_workdir/ $HOME/submit_job.sh 2 $bus $tc
+    	    sbatch -D $HOME/slurm_workdir/ $HOME/aws-parallelcluster-monitoring/run-simulation-scripts/submit_job.sh 2 $bus $tc
         elif [ "$#" -eq 1 ]
 	then
-	    sbatch -D $HOME/slurm_workdir/ $HOME/submit_job.sh $1 $bus $tc
+	    sbatch -D $HOME/slurm_workdir/ $HOME/aws-parallelcluster-monitoring/run-simulation-scripts/submit_job.sh $1 $bus $tc
 	fi
     done
 done
