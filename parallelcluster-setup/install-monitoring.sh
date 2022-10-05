b1e475a4cc34b7233268fa50b6d58d065f5413a7
hinfsynz <szhang.asu@gmail.com>
2022-10-05 10:24:42 -0400

    Update install-monitoring.sh

--- parallelcluster-setup/install-monitoring.sh
+++ parallelcluster-setup/install-monitoring.sh
@@ -97,9 +97,8 @@ case "${cfn_node_type}" in
 		git clone https://github.com/vpenso/prometheus-slurm-exporter.git
 		sed -i 's/NodeList,AllocMem,Memory,CPUsState,StateLong/NodeList: ,AllocMem: ,Memory: ,CPUsState: ,StateLong:/' prometheus-slurm-exporter/node.go
 		cd prometheus-slurm-exporter
-		GOPATH=/root/go-modules-cache HOME=/root go mod download
-		GOPATH=/root/go-modules-cache HOME=/root go build
 		git config --global --add safe.directory ${monitoring_home}/prometheus-slurm-exporter
+		GOPATH=/root/go-modules-cache HOME=/root
 		make
 		mv ${monitoring_home}/prometheus-slurm-exporter/bin/prometheus-slurm-exporter /usr/bin/prometheus-slurm-exporter
 
