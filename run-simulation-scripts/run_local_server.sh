c698afce4810d5c03581346b9f1726af86497d71
Song Zhang <songaws@amazon.com>
2022-10-20 11:32:36 -0400

    use an env variable defined in parent shell

--- run-simulation-scripts/run_local_server.sh
+++ run-simulation-scripts/run_local_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup python3 -u $HOME/aws-parallelcluster-monitoring/run-simulation-scripts/local_http_server.py >> $HOME/local_server_output.log &
+nohup python3 -u ${monitoring_home}/run-simulation-scripts/local_http_server.py >> /tmp/local_server_output.log &
